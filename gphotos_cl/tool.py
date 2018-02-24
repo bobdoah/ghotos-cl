0bb87b8ad053d6c2c555415e64b6c6680d2bd468
bobdoah <robertwilliams1985@gmail.com>
2018-02-24 11:53:23 +0000

    Relative imports don't appear to work when this is executed as a tool. Not quite sure how it did work when I was running the tool before.

--- gphotos_cl/tool.py
+++ gphotos_cl/tool.py
@@ -1,14 +1,14 @@
 import click
 
-import album
-import albums
-import authorized_session 
+import gphotos_cl.album
+import gphotos_cl.albums
+import gphotos_cl.authorized_session 
 
 
 @click.group()
 def entrypoint():
     pass
 
-entrypoint.add_command(album.album)
-entrypoint.add_command(albums.albums)
-entrypoint.add_command(authorized_session.authorize)
+entrypoint.add_command(gphotos_cl.album.album)
+entrypoint.add_command(gphotos_cl.albums.albums)
+entrypoint.add_command(gphotos_cl.authorized_session.authorize)
